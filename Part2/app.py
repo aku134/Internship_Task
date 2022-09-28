ec4cf9d634d8a01e676d415234f28dacb42f7084
Akshita Verma <akshitaverma102001@gmail.com>
2022-09-28 22:13:11 +0530

    visualization of polygon based on coordinates feature added

--- Part2/app.py
+++ Part2/app.py
@@ -1,8 +1,31 @@
 
-# Function to find the leftmost point's index
-def leftmost_point_index(coordinates):
+from tkinter import Tk, Canvas, Frame, BOTH
+
+# Draw polygon shape based on coordinates
+class polygon(Frame):
+
+    def __init__(self,coordinates):
+        super().__init__()
+
+        self.draw_polygon(coordinates)
+
+
+    def draw_polygon(self,coordinates):
+
+        self.master.title("Shapes")
+        self.pack(fill=BOTH, expand=1)
+
+        canvas = Canvas(self)
+        
+        canvas.create_polygon(coordinates, outline='black',
+            fill='blue', width=3)
+
+        canvas.pack(fill=BOTH, expand=1)
+
+# Function to find the leftmost coordinates's index
+def leftmost_coordinates_index(coordinates,n):
     min_val=0
-    for i in range(len(coordinates)):
+    for i in range(n):
         if(coordinates[i][0]<coordinates[min_val][0]):
             min_val=i
         elif(coordinates[i][0]==coordinates[min_val][0]):
@@ -11,10 +34,10 @@ def leftmost_point_index(coordinates):
 
     return min_val
 
-# Function to find the rightmost point's index
-def rightmost_point_index(coordinates):
+# Function to find the rightmost coordinates's index
+def rightmost_coordinates_index(coordinates,n):
     max_val=0
-    for i in range(len(coordinates)):
+    for i in range(n):
         if(coordinates[i][0]>coordinates[max_val][0]):
             max_val=i
         elif(coordinates[i][0]==coordinates[max_val][0]):
@@ -23,15 +46,14 @@ def rightmost_point_index(coordinates):
 
     return max_val
 
-
-def draw_polygon(coordinates):
-    L=leftmost_point_index(coordinates)
-    R=rightmost_point_index(coordinates)
+def ordered_points(coordinates,n):
+    L=leftmost_coordinates_index(coordinates,n)
+    R=rightmost_coordinates_index(coordinates,n)
     A=[]
     B=[]
     C=[]
-    for i in range(len(coordinates)):
-        # If index equals leftmost point or rightmost point
+    for i in range(n):
+        # If index equals leftmost coordinate's index or rightmost coordinate's index
         # skip the current iteration and move to next
         if(i==L or i==R):
             continue
@@ -43,7 +65,7 @@ def draw_polygon(coordinates):
         y2=coordinates[R][1]-coordinates[i][1]
         # If cross porduct is positive the point
         # lies above LR segment
-        # else the point lies below LR
+        # else the point lies below LR segment
         if((x2*y1-x1*y2) >0):
             A.append(coordinates[i])
         elif((x2*y1-x1*y2)<0):
@@ -69,15 +91,29 @@ def draw_polygon(coordinates):
     return ordered_list
 
 coordinates=[]
-coordinates.append((3,2))
-coordinates.append((5,3))
-coordinates.append((2,2))
-coordinates.append((3,1))
-coordinates.append((4,1))
-coordinates.append((3,5))
-# coordinates.append((5,3))
-# coordinates.append((4,3))
-# coordinates.append((3,4))
-# coordinates.append((2,3))
+coordinates.append((300,200))
+coordinates.append((500,300))
+coordinates.append((200,200))
+coordinates.append((300,100))
+coordinates.append((400,100))
+coordinates.append((300,500))
 
-print(draw_polygon(coordinates)) 
+# coordinates.append((500,300))
+# coordinates.append((400,300))
+# coordinates.append((300,400))
+# coordinates.append((200,300))
+
+# coordinates.append((0,300))
+# coordinates.append((200,200))
+# coordinates.append((100,100))
+# coordinates.append((200,100))
+# coordinates.append((300,0))
+# coordinates.append((0,0))
+# coordinates.append((300,300))
+n=len(coordinates)
+ordered_list=ordered_points(coordinates,n)
+
+root = Tk()
+obj = polygon(ordered_list)
+root.geometry("1024x1024")
+root.mainloop()
